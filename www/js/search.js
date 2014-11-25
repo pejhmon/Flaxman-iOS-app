2f235369efeff0b99e73338a2e00c4a8c44649ab
Eimi Okuno <ae.okuno@me.com>
2014-11-25 19:39:55 +0000

    adding some of js

---
+++ www/js/search.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//$(document).ready(function(){
+//
+// $("p:contains('"+searchedText+"')").each( function( i, element ) {
+//      var content = $(element).text();
+//      content = content.replace( searchedText, '<span class="search-found">' + searchedText + '</span>' );
+//      element.html( content );
+// });
+//
+// .search-found {
+//     text-decoration: underline;
+// }
